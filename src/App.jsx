e1e9ec379a513d8a13c60ca7713f0f025300a89f
johndiazdeveloper <johndiazdevelopers137@gmail.com>
2022-11-17 23:41:18 -0500

    refactor/jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //ejemplo de comentario//
   return (
     <div className="App">
       <header className="App-header">
+
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

c35bedbf3855c21629a3735dab7f953e7a3e15b9
Hele <114836079+HeleNavarro@users.noreply.github.com>
2022-11-24 18:47:25 -0500

    project

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import './Navbar.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Navbar() {
+function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false)
   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
   const toggleNav = () => {
     setToggleMenu(!toggleMenu)
-
   }
+
   useEffect(() => {
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
@@ -21,17 +22,20 @@ export default function Navbar() {
 
   return (
     <nav>
-   {/*<img className="logo" src="https://i.postimg.cc/DZr3wYcg/imge-auto.jpg" alt="" />*/}
+   <img className="logo" src="https://i.postimg.cc/3NtTL4nK/au1.png" alt="" />
       {(toggleMenu || screenWidth > 500) && (
       <ul className="list">
       <li className="items">HOME</li>
       <li className="items">SERVICES</li>
       <li className="items">CATALOGUE</li>
+      <li className="items">PROMOTIONES</li>
       <li className="items">CONTACT</li>
-    </ul>
+     </ul>
       )}
-
-      <button onClick={toggleNav} className="btn"> ≡ </button>
+      <div onClick={toggleNav} className="btn">
+      <MenuIcon/>
+      </div>
     </nav>
   )
 }
+export default Navbar;

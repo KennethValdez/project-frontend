01ee8bdf5db6af4e4c5857333296a1058b31094e
jhossue <jhossue_2461@hotmail.com>
2022-12-12 14:21:09 -0500

    estructura

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Navbar from "./navbar/Navbar";
-import Tabs from "./tabmenu/tab";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
